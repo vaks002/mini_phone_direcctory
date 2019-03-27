a74dd835277c4d97f103ab49bfc8410062db1a51
vikram <meenavikram002@gmail.com>
2019-03-27 11:55:45 +0530

    first commit

---
+++ precache-manifest.8345738c2ebd3568c5d00cf7db1f6767.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a697fcc4ed0577334cda3f9df06f43e5",
+    "url": "/mini_phone_directory/static/media/ring2.a697fcc4.gif"
+  },
+  {
+    "revision": "1fab100a1ed0011ad69074e2a85085c0",
+    "url": "/mini_phone_directory/static/media/abs.1fab100a.svg"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/mini_phone_directory/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/mini_phone_directory/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/mini_phone_directory/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "1c71865aabaa15d60b87",
+    "url": "/mini_phone_directory/static/js/runtime~main.13e5ee8e.js"
+  },
+  {
+    "revision": "a1c3e75a37e0f272929f",
+    "url": "/mini_phone_directory/static/js/main.76b8886e.chunk.js"
+  },
+  {
+    "revision": "1a6278a2a6af7af647c8",
+    "url": "/mini_phone_directory/static/js/2.a9131e94.chunk.js"
+  },
+  {
+    "revision": "a1c3e75a37e0f272929f",
+    "url": "/mini_phone_directory/static/css/main.96155f5e.chunk.css"
+  },
+  {
+    "revision": "f645ee832dc763db8080e37de5de78bb",
+    "url": "/mini_phone_directory/index.html"
+  }
+];
